7dfeb0090c2df086eedcc6f9050d122aebf21fd4
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-10-01 21:26:14 +0600

    Two Sum

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -2,9 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
         for i, value in enumerate(nums):
-            remaining = target - nums[i]
+            remain = target - nums[i]
 
             if remaining in seen:
-                return [i, seen[remaining]]
+                return [i, seen[remain]]
 
             seen[value] = i
