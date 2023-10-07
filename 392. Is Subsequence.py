b0597a8928dc11b51c567f29b39ffa75426800ba
Arafat Mahmud <104220863+arafat-mahmud@users.noreply.github.com>
2023-10-07 10:58:45 +0600

    Solve

---
+++ 392. Is Subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        if len(s) <= 0:
+            return True
+        
+        a, b = 0, 0
+        while b < len(t):
+            if s[a] == t[b]:
+                a += 1
+                b += 1
+                if a >= len(s):
+                    return True
+            else:
+                b += 1
+        
+        return False
+    
